3148f7d695d332a7711262b79f94f8cc10f9854a
yangchris11 <b03901086@ntu.edu.tw>
2017-10-27 22:53:16 +0800

    fixed sh

--- hw2/hw2_best.sh
+++ hw2/hw2_best.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-python3 hw2_best.sh $1 $2 $3 $4 $5 $6
+python3 hw2_best.py $1 $2 $3 $4 $5 $6
--- hw2/hw2_logistic.sh
+++ hw2/hw2_logistic.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-python3 hw2_logistic.sh $1 $2 $3 $4 $5 $6
+python3 hw2_logistic.py $1 $2 $3 $4 $5 $6
