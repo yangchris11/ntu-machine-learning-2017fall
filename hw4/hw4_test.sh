ca451a0f897e5d5ab7c9dcac7a7499f540a5aebc
yangchris11 <b03901086@ntu.edu.tw>
2017-12-08 20:17:09 +0800

    model

--- hw4/hw4_test.sh
+++ hw4/hw4_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 wget -O ./model/checkpoint_whole_model.h5 "https://www.dropbox.com/s/sh9khpn3v6zhtny/hw4_checkpoint_whole_model.h5?dl=1"
-python3 hw4_test.py $1
+python3 hw4_test.py $1 $2
