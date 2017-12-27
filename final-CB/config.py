59608bf3944c420be52bd994190f0bae0cb549a7
yangchris11 <b03901086@ntu.edu.tw>
2017-12-27 17:17:21 +0800

    config

--- final-CB/config.py
+++ final-CB/config.py
@@ -3,13 +3,27 @@ import argparse
 
 
 option_num = 6 
+sentences_path = 'data/sentences.pickle'
 
+# word embedding model config
 word_embedding_model_dim = 500
 word_embedding_model_win = 16
 word_embedding_model_min = 3
 jieba_dict_path = 'data/dict.txt.big'
 word_embedding_model_path = 'model/word_embedding_model.bin'
 
+# jieba-tokenizer config
+jieba_tokenizer_path = 'data/jieba_tokenizer.pickle'
+
+# input sequence config 
+max_length = 12
+
+
+
+
+
+
 pred_file = 'submission/pred'
 
 do_predict = True
+
